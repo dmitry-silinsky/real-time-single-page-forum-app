e955e9267caead82b55fc5faec02c3f3b835a751
Dmitry Silinsky <sil.dmitrij@gmail.com>
2019-05-18 23:44:44 +0300

    User model file moved to Models directory

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2019-05-18 19:49:01.
+    * Generated on 2019-05-18 20:44:00.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
